9dcba8c38a8a796304b9192ec2722ce2f96f59b6
Kairat <kairatikernisov@gmail.com>
2022-02-22 19:48:09 +0600

    added pull request

---
+++ src/main/java/com/digital_nomads/javaFall2021/PullRequest/PullRequest.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.javaFall2021.PullRequest;
+
+public class PullRequest {
+    public static void main(String[] args) {
+        System.out.println("Testing pull Request");
+    }
+}
